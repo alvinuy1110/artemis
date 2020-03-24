7e538961e1b83934e6327a973d394d2b984bd6d2
Alvin Uy <alvin_uy@otpp.com>
2020-03-24 17:42:12 -0400

    Add hawtio for UI support

--- consumer/src/main/java/com/myproject/artemis/listener/MyListener.java
+++ consumer/src/main/java/com/myproject/artemis/listener/MyListener.java
@@ -2,12 +2,14 @@ package com.myproject.artemis.listener;
 
 import com.myproject.artemis.exception.MQException;
 import com.myproject.artemis.service.MessagingService;
+import com.myproject.artemis.util.JmsMessageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 import static com.myproject.artemis.service.MessagingConstants.JMS_HEADER_CUSTOM_MSG_ID;
 
@@ -21,9 +23,19 @@ public class MyListener implements MessageListener {
     public void onMessage(Message message) {
 
         try {
-            log.info("Message received {}", message.getBody(Object.class));
+            log.info("Message received");
 
-            messagingService.add(message.getStringProperty(JMS_HEADER_CUSTOM_MSG_ID), message);
+            JmsMessageUtil.debugProperties(message);;
+
+            String customMsgId = message.getStringProperty(JMS_HEADER_CUSTOM_MSG_ID);
+            if (customMsgId!=null) {
+                messagingService.add(customMsgId, message);
+
+            }
+            if (message instanceof TextMessage) {
+                TextMessage textMessage = (TextMessage) message;
+                log.info("Message body {}", textMessage.getText());
+            }
 
         } catch (JMSException e) {
             throw new MQException(e);
